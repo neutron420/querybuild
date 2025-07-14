2ceff80e5082241434ae4e79f04836330e4c358b
Ritesh-o4coder <fnaticritesh2004@gmail.com>
2025-07-15 01:24:48 +0530

    no change

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,4 +2,26 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      maxWidth: {
+        container: "1280px",
+      },
+      animation: {
+        marquee: 'marquee var(--duration) linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(calc(-100% - var(--gap)))' }
+        }
+      }
+    },
+  },
+}
+
+
 export default config;
+
