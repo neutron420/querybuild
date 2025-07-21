b550fa3e7b9bf06007757944302442094a0c2071
Ritesh-o4coder <fnaticritesh2004@gmail.com>
2025-07-21 23:36:49 +0530

    footet added

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage(){
+   return (<div>
+    <h1>Dshbaord</h1>
+    <p>Welcome to the dashboardpage</p>
+    <p>This is the dashboard for the query build  </p>
+   </div>)
+}
