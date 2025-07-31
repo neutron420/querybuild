8229c43a05d6d6ab99bdf55f0fbb13cabba7dfd7
Ritesh-o4coder <fnaticritesh2004@gmail.com>
2025-07-31 12:07:49 +0530

    added ai and popup

---
+++ app/ai/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Aipage() {
+  return (
+    <div className="justify-center text-white *:flex flex-col items-center h-screen background-gradient-to-r from-purple-500 to-pink-500">
+      <h1>AI Page</h1>
+      <p>This is the AI page content.</p>
+      <p>Welcome to the Ai page of tHE Queru Build</p>
+      <p> Where ideas meets excellence</p>\
+    </div>
+  );
+}
---
+++ app/popup/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Popup(){
+  return(
+    <div className=" text-white *:flex flex-col items-center h-screen background-gradient-to-r from-purple-500 to-pink-500">
+        <h1>Welcome to Query Build</h1>
+    </div>
+  );
+}
